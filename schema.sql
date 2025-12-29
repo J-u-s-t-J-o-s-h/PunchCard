20b24aad2b3d4b019845d17551e50c0898dc49bb
JustJosh <n/a>
2025-12-29 12:44:52 -0500

    Update schema to include user trigger

--- schema.sql
+++ schema.sql
@@ -119,3 +119,17 @@ begin
     return R * c;
 end;
 $$ language plpgsql immutable;
+
+-- TRIGGER for auto-creating profile on signup
+create or replace function public.handle_new_user()
+returns trigger as $$
+begin
+  insert into public.profiles (id, full_name, avatar_url, role)
+  values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url', 'employee');
+  return new;
+end;
+$$ language plpgsql security definer;
+
+create or replace trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
